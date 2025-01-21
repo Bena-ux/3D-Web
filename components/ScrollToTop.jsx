1e7ce006d41e5f1db009b98c563e087f84712521
Bena-ux <alessandrobenagli@gmail.com>
2025-01-21 08:21:20 +0100

    Update ScrollToTop.jsx

--- components/ScrollToTop.jsx
+++ components/ScrollToTop.jsx
@@ -5,7 +5,7 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0); // Scroll to the top when the pathname changes
+    window.scrollTo(0, 0); 
   }, [pathname]);
 
   return null;
